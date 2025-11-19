d9447c835524bc60fdd931bf1e80917681775224
rozie97 <ach.rozie@bolt.eu>
2025-11-19 21:08:42 +0200

    refactor staging models

--- models/staging/jaffle_shop/_stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/_stg_jaffle_shop__customers.sql
@@ -1,3 +1,16 @@
-select 
-    * 
-from {{ source('jaffle_shop','jaffle_shop_customers')}}
+with 
+source as (
+    select 
+        * 
+    from {{ source('jaffle_shop','jaffle_shop_customers')}}
+)
+, transformed as (
+    select 
+        id as customer_id,
+        last_name as surname,
+        first_name as givenname,
+        first_name || ' ' || last_name as full_name
+    from source
+)
+
+select * from transformed
