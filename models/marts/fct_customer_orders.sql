ade5d9de1995326b4c68df8693b6cbba37aa564c
rozie97 <ach.rozie@bolt.eu>
2025-11-19 20:05:45 +0200

    beautification and lower case

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -26,13 +26,41 @@ join (
         b.name as full_name,
         b.last_name as surname,
         b.first_name as givenname,
+
         min(order_date) as first_order_date,
-        min(case when a.status NOT IN ('returned','return_pending') then order_date end) as first_non_returned_order_date,
-        max(case when a.status NOT IN ('returned','return_pending') then order_date end) as most_recent_non_returned_order_date,
-        COALESCE(max(user_order_seq),0) as order_count,
-        COALESCE(count(case when a.status != 'returned' then 1 end),0) as non_returned_order_count,
-        sum(case when a.status NOT IN ('returned','return_pending') then ROUND(c.amount/100.0,2) else 0 end) as total_lifetime_value,
-        sum(case when a.status NOT IN ('returned','return_pending') then ROUND(c.amount/100.0,2) else 0 end)/NULLIF(count(case when a.status NOT IN ('returned','return_pending') then 1 end),0) as avg_non_returned_order_value,
+        
+        min(case 
+            when a.status not in ('returned','return_pending') 
+            then order_date 
+        end) as first_non_returned_order_date,
+        
+        max(case 
+            when a.status not in ('returned','return_pending') 
+            then order_date 
+        end) as most_recent_non_returned_order_date,
+        
+        coalesce(max(user_order_seq),0) as order_count,
+        
+        coalesce(count(case
+            when a.status != 'returned' 
+            then 1 end),
+            0) as non_returned_order_count,
+        
+        sum(case 
+            when a.status not in ('returned','return_pending') 
+            then round(c.amount/100.0,2) else 0 
+        end) as total_lifetime_value,
+        
+        sum(case 
+            when a.status not in ('returned','return_pending') 
+            then round(c.amount/100.0,2) else 0 
+        end)
+        /nullif(count(case 
+            when a.status not in ('returned','return_pending') 
+            then 1 end),
+            0
+        ) as avg_non_returned_order_value,
+        
         array_agg(distinct a.id) as order_ids
 
     from (
@@ -53,7 +81,7 @@ join (
     left outer join public.stripe_payments c
     on a.id = c.orderid
 
-    where a.status NOT IN ('pending') and c.status != 'fail'
+    where a.status not in ('pending') and c.status != 'fail'
 
     group by b.id, b.name, b.last_name, b.first_name
 
