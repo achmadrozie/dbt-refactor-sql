fb3987493b59b2a001f0d66dda6e4cf019665a86
rozie97 <ach.rozie@bolt.eu>
2025-11-22 17:29:50 +0200

    logic cte enablement

--- _new_generation/models/marts/fact_customer_orders.sql
+++ _new_generation/models/marts/fact_customer_orders.sql
@@ -9,69 +9,105 @@
     2. SOURCE ENABLEMENT
         - Create Sources
         - Update Sources
+        - Tidy Up
+
+    3. LOGIC CTE ENABLEMENT
+        - Intermediary-ing UP
+        - Simplifying column name modification on CTE logic and move to the source table
+            - payments
+            - orders
+            - customers
 ***/
 -- Source CTE
 with orders as (
-    select *
+    select 
+        *
     from {{ ref('stg_jaffle_shop___orders') }}
 )
 
 , customers as (
-    select *
+    select 
+        *
     from {{ ref('stg_jaffle_shop___customers') }}
 )
-, payments as (
-    select *
+, base_payments as (
+    select
+        *
     from {{ ref('stg_stripe___payments') }}
 )
 -- Logic CTE
+, payments as (
+    select 
+        order_id
+        , max(order_created_date) as payment_finalized_date
+        , sum(order_amount) / 100.0 as total_amount_paid
+    from base_payments
+    where order_status <> 'fail'
+    group by 1
+)
 
 
 -- Final CTE
-, paid_orders as (select orders.id as order_id,
-    orders.user_id	as customer_id,
-    orders.order_date as order_placed_at,
-        orders.status as order_status,
-    p.total_amount_paid,
-    p.payment_finalized_date,
-    c.first_name    as customer_first_name,
-        c.last_name as customer_last_name
-from orders
-left join (select orderid as order_id, max(created) as payment_finalized_date, sum(amount) / 100.0 as total_amount_paid
-        from payments
-        where status <> 'fail'
-        group by 1) p on orders.id = p.order_id
-left join dbt_refactor.public._customers c on orders.user_id = c.id ),
+, paid_orders as (
 
-customer_orders 
-as (select c.id as customer_id
-    , min(order_date) as first_order_date
-    , max(order_date) as most_recent_order_date
-    , count(orders.id) as number_of_orders
-from customers c 
-left join orders
-on orders.user_id = c.id 
-group by 1)
+    select 
+        orders.order_id,
+        orders.customer_id,
+        orders.order_placed_at,
+        orders.order_status,
 
-select
-p.*,
-row_number() over (order by p.order_id) as transaction_seq,
-row_number() over (partition by customer_id order by p.order_id) as customer_sales_seq,
-case when c.first_order_date = p.order_placed_at
-then 'new'
-else 'return' end as nvsr,
-x.clv_bad as customer_lifetime_value,
-c.first_order_date as fdos
-from paid_orders p
-left join customer_orders as c using (customer_id)
-left outer join 
-(
-        select
+        p.total_amount_paid,
+        p.payment_finalized_date,
+
+        c.customer_first_name,
+        c.customer_last_name
+    from orders
+    left join payments p 
+        on orders.order_id = p.order_id
+    left join customers c 
+        on orders.customer_id = c.customer_id 
+
+)
+, customer_lifetime_value as (
+    select
         p.order_id,
         sum(t2.total_amount_paid) as clv_bad
     from paid_orders p
-    left join paid_orders t2 on p.customer_id = t2.customer_id and p.order_id >= t2.order_id
+    left join paid_orders t2 
+        on p.customer_id = t2.customer_id 
+        and p.order_id >= t2.order_id
     group by 1
     order by p.order_id
-) x on x.order_id = p.order_id
-order by order_id
+)
+, customer_orders as (
+
+    select 
+        c.customer_id                                  as customer_id
+        , min(order_placed_at)                         as first_order_date
+        , max(order_placed_at)                         as most_recent_order_date
+        , count(orders.order_id)                       as number_of_orders
+    from customers c 
+    left join orders
+        on orders.customer_id = c.customer_id 
+    group by 1
+)
+
+, final AS (
+    select
+        p.*,
+        row_number() over (order by p.order_id)                                  as transaction_seq,
+        row_number() over (partition by p.customer_id order by p.order_id)         as customer_sales_seq,
+        case when c.first_order_date = p.order_placed_at
+        then 'new'
+        else 'return' end                                                        as nvsr,
+        clv.clv_bad                                                                as customer_lifetime_value,
+        c.first_order_date                                                       as fdos
+    from paid_orders p
+    left join customer_orders as c 
+        on p.customer_id = c.customer_id
+    left join customer_lifetime_value clv
+        on clv.order_id = p.order_id
+    order by order_id
+)
+
+select * from final
--- _new_generation/models/staging/stripe/stg_stripe___payments.sql
+++ _new_generation/models/staging/stripe/stg_stripe___payments.sql
@@ -1,3 +1,7 @@
 select 
-    * 
+    orderid             as order_id
+    , amount            as order_amount
+    , paymentmethod     as payment_method
+    , status            as order_status
+    , created           as order_created_date
 from {{ source("stripe","_payments") }}
