87ae1d647f0ef9f47512205acbc9d6952474a551
rozie97 <ach.rozie@bolt.eu>
2026-03-10 08:40:17 +0700

    enabled schema for staging

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -5,7 +5,8 @@ with source as (
 )
 , transformed as (
     select
-        orderid as order_id
+        id as payment_id
+        , orderid as order_id
         , status as payment_status
         , round(amount/100.0,2) payment_amount
     from source
